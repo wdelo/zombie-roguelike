471c259c872e1c12dbf0892274ea008fd3b29c37
matt-manning <65859640+matt-manning@users.noreply.github.com>
2022-04-17 21:48:48 -0400

    camera movement working well

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class MoveCamera : MonoBehaviour
 {
-    //[SerializeField] private GameObject mainCamera;
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         Vector3 direction = other.transform.position - transform.position;
         //Debug.Log("Object: " + other.transform.name + " Direction: " + direction);
-        //if (gameObject.CompareTag("East") && direction )
+        if (other.gameObject.CompareTag("Player"))
         {
-
+            if (gameObject.CompareTag("East") && direction.x > 0)
+            {
+                mainCamera.transform.Translate(-20f, 0, 0);
+            }
+            else if (gameObject.CompareTag("West") && direction.x < 0)
+            {
+                mainCamera.transform.Translate(20f, 0, 0);
+            }
+            else if (gameObject.CompareTag("South") && direction.z > 0)
+            {
+                mainCamera.transform.Translate(0, -20f, 0);
+            }
+            else if (gameObject.CompareTag("North") && direction.z < 0)
+            {
+                mainCamera.transform.Translate(0, 20f, 0);
+            }
         }
+
     }
 }

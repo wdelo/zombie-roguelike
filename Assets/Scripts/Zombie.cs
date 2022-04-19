5d1778bf8a194f6d69ce00fd676c696c9f76f24f
Oskar Klear <klear.6@buckeyemail.osu.edu>
2022-04-19 19:46:00 -0400

    Fixed rooms, added zombies, etc

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -5,20 +5,31 @@ using UnityEngine.AI;
 
 public class Zombie : MonoBehaviour
 {
-    public Transform target;
+    private Transform target;
     private NavMeshAgent nav;
     private Animator animator;
 
     private void Start()
     {
         nav = this.GetComponent<NavMeshAgent>();
-        animator = this.GetComponent<Animator>();   
+        animator = this.GetComponent<Animator>();
+        target = GameObject.Find("Player").transform;
+        Debug.Log(target);
     }
     // Update is called once per frame
     void Update()
     {
-        nav.SetDestination(target.position);
+        if (Vector3.Distance(transform.position, target.position) < 10)
+        {
+            
+            nav.SetDestination(target.position);
+        }
+        else
+        {
+            //Debug.Log(Vector3.Distance(transform.position, target.position));
+            nav.SetDestination(transform.position);
+        }
         animator.SetFloat("Speed", nav.velocity.magnitude);
-        Debug.Log(nav.velocity.magnitude);
+        //Debug.Log(nav.velocity.magnitude);
     }
 }

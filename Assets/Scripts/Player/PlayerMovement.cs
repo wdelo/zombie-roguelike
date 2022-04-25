a216ea0789ddcde96feb98e01fba864f847508b1
Oskar Klear <klear.6@buckeyemail.osu.edu>
2022-04-25 18:10:55 -0400

    namespace for 1 script

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,69 +1,72 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+namespace Lab6
 {
-    [SerializeField] private float speed = 5f;
-
-    private Animator animator;
-
-    private InputAction moveAction;
-    private InputAction mousePosition;
-
-    private Plane ground;
-
-    public void Initialize(InputAction moveAction, InputAction mousePosition)
+    public class PlayerMovement : MonoBehaviour
     {
-        this.moveAction = moveAction;
-        this.mousePosition = mousePosition;
-    }
+        [SerializeField] private float speed = 5f;
 
-    private void Awake()
-    {
-        ground = new Plane(Vector3.up, 0.0f);
-        animator = GetComponentInChildren<Animator>();
-    }
+        private Animator animator;
 
-    void Update()
-    {
-        Move();
-        Rotate();
-    }
+        private InputAction moveAction;
+        private InputAction mousePosition;
 
-    private void Move()
-    {
-        Vector2 inputVector = moveAction.ReadValue<Vector2>();
-        Vector3 movementVector = new Vector3(inputVector.x, 0, inputVector.y);
-        movementVector.Normalize();
-        transform.position += movementVector * speed * Time.deltaTime;
+        private Plane ground;
 
-        AnimateMove(movementVector);
-    }
-
-    private void AnimateMove(Vector3 movement)
-    {
-        float velocityX = Vector3.Dot(movement, transform.right);
-        float velocityZ = Vector3.Dot(movement, transform.forward);
-
-        animator.SetFloat("VelocityX", velocityX, 0.1f, Time.deltaTime);
-        animator.SetFloat("VelocityZ", velocityZ, 0.1f, Time.deltaTime);
-    }
-
-    private void Rotate()
-    {
-        Vector2 mouseScreenPosition = mousePosition.ReadValue<Vector2>();
-        Ray ray = Camera.main.ScreenPointToRay(mouseScreenPosition);
-
-        float enter;
-        if (ground.Raycast(ray, out enter))
+        public void Initialize(InputAction moveAction, InputAction mousePosition)
         {
-            Vector3 hitPoint = ray.GetPoint(enter);
-            hitPoint.y = transform.position.y;
-            Vector3 direction = hitPoint - transform.position;
+            this.moveAction = moveAction;
+            this.mousePosition = mousePosition;
+        }
 
-            Debug.DrawRay(transform.position, direction, Color.red);
-            Quaternion lookRot = Quaternion.LookRotation(direction);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRot, 10.0f);
+        private void Awake()
+        {
+            ground = new Plane(Vector3.up, 0.0f);
+            animator = GetComponentInChildren<Animator>();
+        }
+
+        void Update()
+        {
+            Move();
+            Rotate();
+        }
+
+        private void Move()
+        {
+            Vector2 inputVector = moveAction.ReadValue<Vector2>();
+            Vector3 movementVector = new Vector3(inputVector.x, 0, inputVector.y);
+            movementVector.Normalize();
+            transform.position += movementVector * speed * Time.deltaTime;
+
+            AnimateMove(movementVector);
+        }
+
+        private void AnimateMove(Vector3 movement)
+        {
+            float velocityX = Vector3.Dot(movement, transform.right);
+            float velocityZ = Vector3.Dot(movement, transform.forward);
+
+            animator.SetFloat("VelocityX", velocityX, 0.1f, Time.deltaTime);
+            animator.SetFloat("VelocityZ", velocityZ, 0.1f, Time.deltaTime);
+        }
+
+        private void Rotate()
+        {
+            Vector2 mouseScreenPosition = mousePosition.ReadValue<Vector2>();
+            Ray ray = Camera.main.ScreenPointToRay(mouseScreenPosition);
+
+            float enter;
+            if (ground.Raycast(ray, out enter))
+            {
+                Vector3 hitPoint = ray.GetPoint(enter);
+                hitPoint.y = transform.position.y;
+                Vector3 direction = hitPoint - transform.position;
+
+                Debug.DrawRay(transform.position, direction, Color.red);
+                Quaternion lookRot = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRot, 10.0f);
+            }
         }
     }
 }
